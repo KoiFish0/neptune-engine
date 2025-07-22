b400b2c19021a2d952f5ad9cafc7d1656dec31df
KoiFish0 <koltonweppler@gmail.com>
2025-07-21 22:24:50 -0400

    More lighting

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,6 @@
 /* MAIN.CPP
  *
- * A little testing file. Somewhat of a sample file.
+ * A little testing file. 
  *
  * */
 
@@ -21,45 +21,76 @@ int main(void) {
 //  Engine::wireframeView(true);
 
   // Load and create a shader program from a file
-  Shader shaderProgram("src/shaders/presets/phongTextureSpecMap.vert", "src/shaders/presets/phongTextureSpecMap.frag");
 
-  unsigned int diffuse = Texture::loadTextureRGBA("assets/container2.png", 1);
+  glm::vec3 cubePositions[] = {
+    glm::vec3( 0.0f,  0.0f,  0.0f),
+    glm::vec3( 2.0f,  5.0f, -15.0f),
+    glm::vec3(-1.5f, -2.2f, -2.5f),
+    glm::vec3(-3.8f, -2.0f, -12.3f),
+    glm::vec3( 2.4f, -0.4f, -3.5f),
+    glm::vec3(-1.7f,  3.0f, -7.5f),
+    glm::vec3( 1.3f, -2.0f, -2.5f),
+    glm::vec3( 1.5f,  2.0f, -2.5f),
+    glm::vec3( 1.5f,  0.2f, -1.5f),
+    glm::vec3(-1.3f,  1.0f, -1.5f)
+  };
+
+  Shader shaderProgram("src/shaders/presets/phongTextureSpecMapPoint.vert", "src/shaders/presets/phongTextureSpecMapPoint.frag");
+
   unsigned int specMap = Texture::loadTextureRGBA("assets/container2_specular.png", 0);
+  unsigned int diffuse = Texture::loadTextureRGBA("assets/container2.png", 1);
 
-  Cube cube = Cube::create(shaderProgram.ID);
-  Cube cube2 = Cube::create(shaderProgram.ID);
+//  Cube cube = Cube::create(shaderProgram.ID);
+//  Cube cube2 = Cube::create(shaderProgram.ID);
+
+  for(unsigned int i = 0; i < 10; i++) {
+    Cube randomCube = Cube::create(shaderProgram.ID);
+    Cubes[i].pos = cubePositions[i];
+    float angle = 20.0f * (i + 1);
+    Cubes[i].rotation = glm::vec3(angle, angle, angle);
+    Cubes[i].texture1 = diffuse;
+    Cubes[i].texture2 = specMap;
+  }
 
   // Assign textures to be used in the draw call of the corresponding cube
-  Cubes[0].texture1 = diffuse;
-  Cubes[1].texture1 = diffuse;
-  Cubes[0].texture2 = specMap;
-  Cubes[1].texture2 = specMap;
-
-  Cubes[1].pos.x = 1.5f;
-  Cubes[1].pos.z = -1.5f;
+//  Cubes[0].texture1 = diffuse;
+//  Cubes[1].texture1 = diffuse;
+//  Cubes[0].texture2 = specMap;
+//  Cubes[1].texture2 = specMap;
+//
+//  Cubes[1].pos.x = 1.5f;
+//  Cubes[1].pos.z = -1.5f;
+//
+//  Cubes[1].rotation.z = 40.0f;
   
-  glm::vec3 lightPos = glm::vec3(3.0f, 1.0f, 1.5f);
+  glm::vec3 lightPos = glm::vec3(0.0f, 1.5f, 0.0f);
 
   // Set active shader before modifying uniforms
   // TODO Handle it under the hood
   shaderProgram.use();
 
   // Set texture uniform
-  shaderProgram.setInt("material.diffuse", 1);
   shaderProgram.setInt("material.specular", 0);
+  shaderProgram.setInt("material.diffuse", 1);
 
-  shaderProgram.setVec3("material.ambient", 1.0f, 0.5f, 0.31f);
+  shaderProgram.setVec3("material.ambient", 1.0f * 0.5f, 0.5f * 0.5f, 0.31f * 0.5f);
 //  shaderProgram.setVec3("material.diffuse", 1.0f, 0.5f, 0.31f);
 //  shaderProgram.setVec3("material.specular", 0.5f, 0.5f, 0.5f);
   shaderProgram.setFloat("material.shininess", 32.0f);
 
   shaderProgram.setVec3("light.ambient",  0.2f, 0.2f, 0.2f);
-  shaderProgram.setVec3("light.diffuse",  0.5f, 0.5f, 0.5f);
+  shaderProgram.setVec3("light.diffuse",  0.8f, 0.8f, 0.8f);
   shaderProgram.setVec3("light.specular", 1.0f, 1.0f, 1.0f); 
 
   shaderProgram.setVec3("objectColor", 1.0f, 0.5f, 0.31f);
   shaderProgram.setVec3("lightColor",  0.8f, 0.8f, 0.8f);
-  shaderProgram.setVec3("lightPos", lightPos);
+//  shaderProgram.setVec3("light.direction", -0.2f, -1.0f, -0.3f); 	
+
+  shaderProgram.setFloat("light.constant",  1.0f);
+  shaderProgram.setFloat("light.linear",    0.09f);
+  shaderProgram.setFloat("light.quadratic", 0.032f);	
+
+  shaderProgram.setVec3("light.position", lightPos); 
 
   Camera camera;
 
@@ -90,6 +121,7 @@ int main(void) {
   // Main loop
   while (!glfwWindowShouldClose(window)) {
     shaderProgram.setVec3("viewPos", activeCamera.position); 
+
     double deltaX = Input::getMouseX() - lastX;
     double deltaY = lastY - Input::getMouseY();
     
@@ -146,7 +178,7 @@ int main(void) {
     }
 
     // FPS
-    //printf("%f\n", 1 / delta);
+//    printf("%f\n", 1 / delta);
 
     Engine::refresh();
   }
--- src/shaders/presets/phongTextureSpecMap.frag
+++ src/shaders/presets/phongTextureSpecMap.frag
@@ -2,7 +2,7 @@
 out vec4 FragColor;
   
 in vec2 TexCoord;
-in vec3 Normal;  
+in vec3 Normal;
 in vec3 FragPos;  
 
 struct Material {
@@ -34,6 +34,7 @@ void main() {
 
   // diffuse 
   vec3 norm = normalize(Normal);
+  vec3 lightDir = normalize(-light.direction);
   vec3 lightDir = normalize(lightPos - FragPos);
   float diff = max(dot(norm, lightDir), 0.0);
   vec3 diffuse = light.diffuse * diff * vec3(texture(material.diffuse, TexCoord));

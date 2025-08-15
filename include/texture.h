afe6fe72d0fb635087648501c4e69c0a7f5d564e
KoiFish0 <koltonweppler@gmail.com>
2025-08-15 18:52:59 -0400

    Simplify texture class

--- include/texture.h
+++ include/texture.h
@@ -9,15 +9,37 @@
 #include "globals.h"
 #include "stb_image/stb_image.h"
 
+/*
+ * TextureType describes what the texture is used for. This is important for
+ * loading 3D models properly. The GENERIC 'tag' is intended to be used by
+ * developers and the rest are mainly for the model loader.
+ */
+enum TextureType {
+  GENERIC,
+  DIFFUSE,
+  SPECULAR,
+  NORMAL,
+  HEIGHT_MAP
+};
+
 class Texture {
 public:
-  static unsigned int loadTextureRGB(const char *path, int slot) {
-    unsigned int textureRGB;
-    glGenTextures(1, &textureRGB);
-    glActiveTexture(GL_TEXTURE0 + slot); /* slots are defined as an int starting at GL_TEXTURE0 so adding an int to it brings it to a slot of 0 + int */
-    glBindTexture(GL_TEXTURE_2D, textureRGB);
+  unsigned int texture;
+  enum TextureType type;
 
-    /* set the texture wrapping/filtering options (on the currently bound texture object) */
+  /* TextureType has no effect outside of loading models. Use GENERIC if unsure. */
+  Texture(const char* path, int slot, bool containsAlpha, enum TextureType type) {
+    this->type = type;
+    glGenTextures(1, &texture);
+    /*
+     * Slots are defined as integers starting at GL_TEXTURE0 (0x84c0).
+     * Incrementing GL_TEXTURE0 gives GL_TEXTURE#, where: # = the number that is
+     * incremented by.
+     */
+    glActiveTexture(GL_TEXTURE0 + slot); 
+    glBindTexture(GL_TEXTURE_2D, texture);
+
+    /* Set the texture wrapping/filtering options */
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);	
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
     glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
@@ -27,55 +49,17 @@ public:
     stbi_set_flip_vertically_on_load(true);  
     unsigned char *data = stbi_load(path, &width, &height, &nrChannels, 0);
     if (data) {
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, width, height, 0, GL_RGB, GL_UNSIGNED_BYTE, data);
+      glTexImage2D(GL_TEXTURE_2D, 0, containsAlpha ? GL_RGBA : GL_RGB, width, height, 0, containsAlpha ? GL_RGBA : GL_RGB, GL_UNSIGNED_BYTE, data);
       glGenerateMipmap(GL_TEXTURE_2D);
       if (debugPrint == true) {
-        std::cout << "Loaded texture: " << path << " (ID: " << textureRGB << ")" << std::endl;
+        std::cout << "Loaded texture: " << path << " (ID: " << texture << ")" << std::endl;
       }
     } else {
       std::cout << "Failed to load texture: " << path << " (" << stbi_failure_reason() << ")" << std::endl;
     }
 
     stbi_image_free(data);
-
-    return textureRGB;
-  }
-
-  static unsigned int loadTextureRGBA(const char *path, int slot) {
-    unsigned int textureRGBA;
-    glGenTextures(1, &textureRGBA);
-    glActiveTexture(GL_TEXTURE0 + slot);
-    glBindTexture(GL_TEXTURE_2D, textureRGBA);
-
-    /** 
-     * Some texture parameters
-     * TODO: Allow selection of these parameters through this method
-     */
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
-    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
-
-    int width, height, nrChannels;
-    stbi_set_flip_vertically_on_load(true);
-    unsigned char *data = stbi_load(path, &width, &height, &nrChannels, STBI_rgb_alpha);
-    if (data) {
-      glTexImage2D(GL_TEXTURE_2D, 0, GL_RGBA, width, height, 0, GL_RGBA, GL_UNSIGNED_BYTE, data);
-      glGenerateMipmap(GL_TEXTURE_2D);
-      if (debugPrint == true) {
-        std::cout << "Loaded texture: " << path << " (ID: " << textureRGBA << ")" << std::endl;
-      }
-    } else {
-      std::cout << "Failed to load texture: " << path << " (" << stbi_failure_reason() << ")" << std::endl;
-    }
-    stbi_image_free(data);
-
-    /* Check OpenGL errors */
-    while (GLenum error = glGetError()) {
-      std::cout << "OpenGL Error in loadTextureRGBA: " << error << std::endl;
-    }
-
-    return textureRGBA;
   }
 };
+
 #endif 
